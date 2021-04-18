bd22c6891ff458fd9a33eab1c8133206ba80f45b
rodolfo domingos <rodolfo.dmn@gmail.com>
2021-04-18 18:54:18 -0300

    limitando banners

--- wnarde/app/js/banner/layers.js
+++ wnarde/app/js/banner/layers.js
@@ -2,151 +2,151 @@ import content from '../content.js'
 import touch from './touch.js'
 import movement from './movement.js'
 const bannerLayers = {
-	fps: 30,
-	mY: 0,
-	mX: 0,
-	mYold: 0,
-	mXold: 0,
-	changScreen: 0,
-	cleanInterval: false,
-	screen: window.screen,
-	middle: window.screen.width / 2,
-	center: window.screen.height / 2,
-	init: function (layer) {
-		var base = bannerLayers.createBanner(layer)
-		if (window.screen.width > 800) {
-			bannerLayers.mouseMove(document.querySelector('.banner-content'))
-		} else {
-			bannerLayers.moveScreen()
-		}
-		return base
-	},
-	mouseMove: function () {
-		window.onmousemove = function (e) {
-			bannerLayers.mX = e.clientX
-			bannerLayers.mY = e.clientY
-		}
-		var moveInterval = setInterval(function () {
-			if (bannerLayers.cleanInterval) {
-				clearInterval(moveInterval)
-			}
-			bannerLayers.cleanInterval = false
-			if (bannerLayers.mX > bannerLayers.mXold) {
-				movement.toRight()
-			} else if (bannerLayers.mX < bannerLayers.mXold) {
-				movement.toLeft()
-			}
-			if (bannerLayers.mY > bannerLayers.mYold) {
-				movement.toUp()
-			} else if (bannerLayers.mY < bannerLayers.mYold) {
-				movement.toDown()
-			}
-			bannerLayers.mXold = bannerLayers.mX
-			bannerLayers.mYold = bannerLayers.mY
-		}, 60)
-	},
-	moveScreen: function () {
-		window.ondeviceorientation = function (e) {
-			if (e.gamma > 0) {
-				movement.toLeft()
-			}
-			else if (e.gamma < 0) {
-				movement.toRight()
-			}
+  fps: 30,
+  mY: 0,
+  mX: 0,
+  mYold: 0,
+  mXold: 0,
+  changScreen: 0,
+  cleanInterval: false,
+  screen: window.screen,
+  middle: window.screen.width / 2,
+  center: window.screen.height / 2,
+  init: function (layer) {
+    var base = bannerLayers.createBanner(layer)
+    if (window.screen.width > 800) {
+      bannerLayers.mouseMove(document.querySelector('.banner-content'))
+    } else {
+      bannerLayers.moveScreen()
+    }
+    return base
+  },
+  mouseMove: function () {
+    window.onmousemove = function (e) {
+      bannerLayers.mX = e.clientX
+      bannerLayers.mY = e.clientY
+    }
+    var moveInterval = setInterval(function () {
+      if (bannerLayers.cleanInterval) {
+        clearInterval(moveInterval)
+      }
+      bannerLayers.cleanInterval = false
+      if (bannerLayers.mX > bannerLayers.mXold) {
+        movement.toRight()
+      } else if (bannerLayers.mX < bannerLayers.mXold) {
+        movement.toLeft()
+      }
+      if (bannerLayers.mY > bannerLayers.mYold) {
+        movement.toUp()
+      } else if (bannerLayers.mY < bannerLayers.mYold) {
+        movement.toDown()
+      }
+      bannerLayers.mXold = bannerLayers.mX
+      bannerLayers.mYold = bannerLayers.mY
+    }, 60)
+  },
+  moveScreen: function () {
+    window.ondeviceorientation = function (e) {
+      if (e.gamma > 0) {
+        movement.toLeft()
+      }
+      else if (e.gamma < 0) {
+        movement.toRight()
+      }
 
-		}
-	},
-	cleanMovement: function () {
-		this.cleanInterval = true
-	},
-	needClean: function () {
-		return this.cleanInterval
-	},
-	createBanner: function (layer) {
-		var banner = document.createElement('div')
-		banner.className = 'banner-base'
-		banner.append(this.createCentral(layer.central))
-		layer.secondary.forEach(function (s) {
-			if (s.isPrincipal) {
-				banner.append(bannerLayers.createSecondary(s, true))
-				return
-			}
-			banner.append(bannerLayers.createSecondary(s))
-		})
-		banner.append(this.createText(layer.text))
-		return banner
-	},
-	createCentral: function (layer) {
-		return this.createLayer(layer, 'banner-central')
-	},
-	createSecondary: function (layer, isPrincipal) {
-		return this.createLayer(layer, 'banner-secondary', isPrincipal)
-	},
-	createText: function (layer) {
-		return this.createLayerText(layer, 'banner-text')
-	},
-	createLayer: function (layer, id, isPrincipal) {
-		if (isPrincipal) {
-			id = `${id} principal-banner`
-		}
-		const div = document.createElement('div')
-		const img = document.createElement('img')
-		img.src = `assets/banners/${layer.src}.png`
-		if (window.screen.width < 800)
-			img.src = `assets/bannersMobile/${layer.src}.png`
+    }
+  },
+  cleanMovement: function () {
+    this.cleanInterval = true
+  },
+  needClean: function () {
+    return this.cleanInterval
+  },
+  createBanner: function (layer) {
+    var banner = document.createElement('div')
+    banner.className = 'banner-base'
+    banner.append(this.createCentral(layer.central))
+    layer.secondary.forEach(function (s) {
+      if (s.isPrincipal) {
+        banner.append(bannerLayers.createSecondary(s, true))
+        return
+      }
+      banner.append(bannerLayers.createSecondary(s))
+    })
+    banner.append(this.createText(layer.text))
+    return banner
+  },
+  createCentral: function (layer) {
+    return this.createLayer(layer, 'banner-central')
+  },
+  createSecondary: function (layer, isPrincipal) {
+    return this.createLayer(layer, 'banner-secondary', isPrincipal)
+  },
+  createText: function (layer) {
+    return this.createLayerText(layer, 'banner-text')
+  },
+  createLayer: function (layer, id, isPrincipal) {
+    if (isPrincipal) {
+      id = `${id} principal-banner`
+    }
+    const div = document.createElement('div')
+    const img = document.createElement('img')
+    img.src = `assets/banners/${layer.src}.png`
+    if (window.screen.width < 800)
+      img.src = `assets/bannersMobile/${layer.src}.png`
 
-		img.onload = function () {
-			this.style.position = 'absolute'
-			this.style.left = (window.screen.width / 2 - this.width / 2) - layer.pos.left
-			this.style.top = layer.pos.top
-		}
+    img.onload = function () {
+      this.style.position = 'absolute'
+      this.style.left = (window.screen.width / 2 - this.width / 2) - layer.pos.left
+      this.style.top = layer.pos.top
+    }
 
-		div.append(img)
-		div.className = id
-		img.style.cursor = 'pointer'
-		img.parentElement.onclick = function () {
-			if (touch.transitionDone) {
-				content.showContent()
-				document.body.className = document.body.classList[0]
-			}
-		}
-		return div
-	},
+    div.append(img)
+    div.className = id
+    img.style.cursor = 'pointer'
+    img.parentElement.onclick = function () {
+      if (touch.transitionDone) {
+        content.showContent()
+        document.body.className = document.body.classList[0]
+      }
+    }
+    return div
+  },
 
-	createLayerText: function (layer, id) {
-		const div = document.createElement('div')
-		const p = []
-		layer.forEach(function (text, key) {
-			var t_ = document.createElement('p')
-			if (typeof text.pre !== 'undefined') {
-				t_ = document.createElement('pre')
-			}
-			t_.innerHTML = text.text
-			t_.style.cursor = 'pointer'
-			t_.className = text.class
-			p.push(t_)
-			t_.onclick = function () {
-				if (touch.transitionDone) {
-					content.showContent()
-					document.body.className = document.body.classList[0]
-				}
-			}
-			div.append(t_)
+  createLayerText: function (layer, id) {
+    const div = document.createElement('div')
+    const p = []
+    layer.forEach(function (text, key) {
+      var t_ = document.createElement('p')
+      if (typeof text.pre !== 'undefined') {
+        t_ = document.createElement('pre')
+      }
+      t_.innerHTML = text.text
+      t_.style.cursor = 'pointer'
+      t_.className = text.class
+      p.push(t_)
+      t_.onclick = function () {
+        if (touch.transitionDone) {
+          content.showContent()
+          document.body.className = document.body.classList[0]
+        }
+      }
+      div.append(t_)
 
-			t_.style.position = 'absolute'
-			t_.style.left = window.screen.width / 2 - layer[key].pos.left
-			t_.style.top = layer[key].pos.top
+      t_.style.position = 'absolute'
+      t_.style.left = window.screen.width / 2 - layer[key].pos.left
+      t_.style.top = layer[key].pos.top
 
-		})
+    })
 
-		div.className = id
-		return div
-	},
-	setPorcent: function () {
-		return this.middle / this.toMove
-	},
-	checkDistance: function (mousePos) {
-		return parseInt(mousePos / this.setPorcent())
-	},
+    div.className = id
+    return div
+  },
+  setPorcent: function () {
+    return this.middle / this.toMove
+  },
+  checkDistance: function (mousePos) {
+    return parseInt(mousePos / this.setPorcent())
+  },
 }
 export default bannerLayers
--- wnarde/app/js/banner/movement.js
+++ wnarde/app/js/banner/movement.js
@@ -3,61 +3,97 @@ import bannerLayers from './layers.js'
 const movement = {
 	c: '',
 	s: '',
-	limit: 20,
+	limit: 40,
+	movements: 0,
 	porcent: 0,
 	canMove: true,
+	canGo: {
+		r: 0,
+		l: 0,
+		u: 0,
+		d: 0
+	},
 	init: function () {
 		this.c = document.querySelector('.banner-central').querySelector('img')
 		this.s = document.querySelectorAll('.banner-secondary')
 		this.canMove = true
 	},
-	move: function (inC, inS, isVertival) {
+	move: function (inC, inS, isVertival, reverse) {
 		if (isVertival) {
-			if (true) {
-				movement.doMovimentV(inC, inS)
-			}
-			if (true) {
-				movement.doMovimentV(inC, inS)
-			}
-		} else {
-			if (true) {
-				movement.doMovimentH(inC, inS)
-			}
-			if (true) {
-				movement.doMovimentH(inC, inS)
-			}
+			return movement.doMovimentV(inC, inS, reverse)
 		}
+		return movement.doMovimentH(inC, inS, reverse)
 	},
 	toRight: function () {
-		this.move(-1, -1, false)
+		if (movement.canGo.r < movement.limit) {
+			this.move(1, 1, false, true)
+			if (movement.canGo.l > 0) {
+				movement.canGo.l--
+			}
+			movement.canGo.r++
+		}
 	},
 	toLeft: function () {
-		this.move(1, 1, false)
+		if (movement.canGo.l < movement.limit) {
+			this.move(1, 1, false, false)
+			if (movement.canGo.r > 0) {
+				movement.canGo.r--
+			}
+			movement.canGo.l++
+		}
 	},
 	toUp: function () {
-		this.move(-1, -1, true, false)
+		if (movement.canGo.u < movement.limit) {
+			this.move(1, 1, true, true)
+			if (movement.canGo.d > 0) {
+				movement.canGo.d--
+			}
+			movement.canGo.u++
+		}
 	},
 	toDown: function () {
-		this.move(1, 1, true, true)
+		if (movement.canGo.d < movement.limit) {
+			this.move(1, 1, true, false)
+			if (movement.canGo.u > 0) {
+				movement.canGo.u--
+			}
+			movement.canGo.d++
+		}
 	},
-	doMovimentH: function (inC, inS) {
+	doMovimentH: function (inC, inS, reverse) {
 		if (this.canMove) {
+			if (reverse) {
+				inC = inC * -1
+				inS = inS * -1
+			}
+
 			movement.c.style.left = `${utils.pxToIn(movement.c.style.left) - inC}px`
 			if (typeof movement.s[0] !== 'undefined')
-				movement.s[0].querySelector('img').style.left = `${utils.pxToIn(movement.s[0].querySelector('img').style.left) - inS}px`
+				movement.s[0].querySelector('img').style.left = `${utils.pxToIn(movement.s[0].querySelector('img').style.left) + inS}px`
 
 			if (typeof movement.s[1] !== 'undefined')
 				movement.s[1].querySelector('img').style.left = `${utils.pxToIn(movement.s[1].querySelector('img').style.left) + inS}px`
 		}
 	},
-	doMovimentV: function (inC, inS) {
+	doMovimentV: function (inC, inS, reverse) {
 		if (this.canMove) {
+			if (reverse) {
+				inC = inC * -1
+				inS = inS * -1
+			}
 			movement.c.style.top = `${utils.pxToIn(movement.c.style.top) - inC}px`
 			if (typeof movement.s[0] !== 'undefined')
-				movement.s[0].querySelector('img').style.top = `${utils.pxToIn(movement.s[0].querySelector('img').style.top) - inS}px`
+				movement.s[0].querySelector('img').style.top = `${utils.pxToIn(movement.s[0].querySelector('img').style.top) + inS}px`
 
 			if (typeof movement.s[1] !== 'undefined')
 				movement.s[1].querySelector('img').style.top = `${utils.pxToIn(movement.s[1].querySelector('img').style.top) + inS}px`
+
+			if (reverse) {
+				movement.movements--
+				return movement.movements >= movement.limit
+			}
+			movement.movements++;
+			return movement.movements >= movement.limit
 		}
 	},
 	stop: function () {
